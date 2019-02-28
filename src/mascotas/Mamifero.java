f0ab0423d286f9ffa5c9b96be90096ae22137a92
dipo597 <48086209+dipo597@users.noreply.github.com>
2019-02-28 19:56:37 +0100

    Cambio a mamifero

--- src/mascotas/Mamifero.java
+++ src/mascotas/Mamifero.java
@@ -2,55 +2,74 @@ package mascota;
 
 import java.time.LocalDate;
 
+/**
+ * @author Lucas González DW1E
+ * <p>
+ * Clase mamífero que extiende mascota.
+ */
 public class Mamifero extends Mascota{
-
-	//propiedades
+	
 	private String chip;
 	
-	/**
-	 * Constructor de la clase Mamifero. Llama al constructor de la clase padre y valida el valor del chip antes de crear el objeto.
-	 * @param nombre (String)
-	 * @param especie (String)
-	 * @param fechaNacimiento (LocalDate)
-	 * @param chip (String)
-	 */
-	public Mamifero(String nombre, String especie, LocalDate fechaNacimiento, String chip){
+	public Mamifero(String nombre, String especie, LocalDate fechaNacimiento, String chip) {
+		
 		super(nombre, especie, fechaNacimiento);
-		if (!chip.matches("^\\d{15}$")) throw new IllegalArgumentException();
-		this.chip=chip;
-	}
-	
-	//métodos
-	@Override
-	public String toString(){
-		String str=super.toString();
-		
-		str+="\n\tMamífero ("+this.getChip()+").\n";
-		
-		return str;
+		setChip(chip);
 	}
 	
 	@Override
-	public LocalDate calcularProximaRevision(){
-		LocalDate prox;
+	public String toString() {
 		
-		if (this.calcularEdad()<18){
-			prox=super.fechaUltimaRevision().plusMonths(3);
-		}
-		else{
-			prox=super.fechaUltimaRevision().plusMonths(6);
-		}
-		
-		return prox;
+		return "Mamífero: \n"+super.toString()+"\nChip: "+getChip();
 	}
 	
-	//getters & setters
+	@Override
+	public LocalDate calcularProximaRevision() {
+
+		LocalDate proximaRevision;
+		
+		int edadAños = getFechaNacimiento().until(LocalDate.now()).getYears(); boolean continuar = true; int i = 0; int b = 0;
+		
+		if (edadAños <= 2) {
+			
+			i=3;
+			b=3;
+		} else {
+			i=6;
+			b=6;
+		}
+		
+		do {
+			
+			if (getFechaNacimiento().plusMonths(i).isBefore(LocalDate.now())) {
+			
+				i+=b;
+			} else {
+				continuar = false;
+			}
+			
+		}while(continuar);
+
+		proximaRevision = getFechaNacimiento().plusMonths(i);
+		
+		return proximaRevision;
+	}
+
 	public String getChip() {
 		return chip;
 	}
 
-	public void setChip(String chip) {
+	private void setChip(String chip) {
+		
+		if (getChip() != null) {
+				
+			throw new IllegalArgumentException("No puedes alterar el chip de un mamífero.");
+		} else if (!chip.matches("\\d{15}$")) {
+			
+			throw new IllegalArgumentException("Chip inválido. Debe tener 15 digitos.");
+		}
+		
 		this.chip = chip;
 	}
-	
+
 }
